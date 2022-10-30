8a7127c0b3a31731077ebd870cd99def9e3f100d
Xartiou <76091221+xartiou@users.noreply.github.com>
2022-10-30 14:35:20 +0300

    Add files via upload

--- vk_db_creation_L_3.sql
+++ vk_db_creation_L_3.sql
@@ -1,18 +1,19 @@
-DROP DATABASE IF EXISTS vk;
-CREATE DATABASE vk;
-USE vk;
+DROP DATABASE IF EXISTS vk; -- удалим если существует
+CREATE DATABASE vk; -- создаем БД
+USE vk;  -- используем БД
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+DROP TABLE IF EXISTS users; -- удалим если существует, что бы обновлять только эту таблицу
+CREATE TABLE users (  -- создаем таблицу
 	id SERIAL PRIMARY KEY, -- SERIAL = BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
     firstname VARCHAR(100),
     lastname VARCHAR(100) COMMENT 'Фамилия', -- COMMENT на случай, если имя неочевидное
     email VARCHAR(100) UNIQUE,
     password_hash varchar(100),
-    phone BIGINT,
+    phone BIGINT UNSIGNED,
     is_deleted bit default 0,
     -- INDEX users_phone_idx(phone), -- помним: как выбирать индексы
-    INDEX users_firstname_lastname_idx(firstname, lastname)
+    -- для поиска по имени и фамилии
+    INDEX users_firstname_lastname_idx(firstname, lastname) -- индексы нужны для сокращения повторений при поиске
 );
 
 DROP TABLE IF EXISTS `profiles`;
@@ -20,8 +21,8 @@ CREATE TABLE `profiles` (
 	user_id SERIAL PRIMARY KEY,
     gender CHAR(1),
     birthday DATE,
-	photo_id BIGINT UNSIGNED NULL,
-    created_at DATETIME DEFAULT NOW(),
+	photo_id BIGINT UNSIGNED NULL, -- ссылаемся на внешний ключ таблицы photo
+    created_at DATETIME DEFAULT NOW(), -- NOW()-системное время
     hometown VARCHAR(100)
     -- , FOREIGN KEY (photo_id) REFERENCES media(id) -- пока рано, т.к. таблицы media еще нет
 );
@@ -32,10 +33,12 @@ CREATE TABLE `profiles` (
 -- SET NULL
 -- SET DEFAULT
 
-
+-- создаем связь 1 к 1 users и profile
 ALTER TABLE `profiles` ADD CONSTRAINT fk_user_id
     FOREIGN KEY (user_id) REFERENCES users(id)
-    ON UPDATE CASCADE ON DELETE CASCADE;
+    ON UPDATE CASCADE ON DELETE CASCADE; -- при изменении и при удалении
+    
+    
 
 DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
@@ -44,37 +47,40 @@ CREATE TABLE messages (
     to_user_id BIGINT UNSIGNED NOT NULL,
     body TEXT,
     created_at DATETIME DEFAULT NOW(), -- можно будет даже не упоминать это поле при вставке
-
+-- создаем связь 1 к многим
     FOREIGN KEY (from_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (to_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+-- заявки на дружбу
 DROP TABLE IF EXISTS friend_requests;
 CREATE TABLE friend_requests (
 	-- id SERIAL PRIMARY KEY, -- изменили на составной ключ (initiator_user_id, target_user_id)
 	initiator_user_id BIGINT UNSIGNED NOT NULL,
     target_user_id BIGINT UNSIGNED NOT NULL,
-    -- `status` TINYINT UNSIGNED,
-    `status` ENUM('requested', 'approved', 'declined', 'unfriended'),
-    -- `status` TINYINT UNSIGNED, -- в этом случае в коде хранили бы цифирный enum (0, 1, 2, 3...)
-	requested_at DATETIME DEFAULT NOW(),
+    	-- `status` TINYINT UNSIGNED,
+    	-- `status` TINYINT UNSIGNED, -- в этом случае в коде хранили бы цифирный enum (0, 1, 2, 3...)
+	`status` ENUM('requested', 'approved', 'declined', 'unfriended'),
+    requested_at DATETIME DEFAULT NOW(),
 	updated_at DATETIME on update now(),
 	
-    PRIMARY KEY (initiator_user_id, target_user_id),
+    PRIMARY KEY (initiator_user_id, target_user_id), -- составной ключ, что бы не было парных заявок на дружбу
     FOREIGN KEY (initiator_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (target_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+
+-- таблица группы (связь многие ко многим)
 DROP TABLE IF EXISTS communities;
 CREATE TABLE communities(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(150),
 	admin_user_id BIGINT UNSIGNED,
 
-	INDEX communities_name_idx(name),
+	INDEX communities_name_idx(name), -- индекс для помска по имени группы
 	FOREIGN KEY (admin_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
-
+-- связующая таблица для связи юзера и группы (связь многие ко многим)
 DROP TABLE IF EXISTS users_communities;
 CREATE TABLE users_communities(
 	user_id BIGINT UNSIGNED NOT NULL,
@@ -85,6 +91,7 @@ CREATE TABLE users_communities(
     FOREIGN KEY (community_id) REFERENCES communities(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+-- таблица справочник для media
 DROP TABLE IF EXISTS media_types;
 CREATE TABLE media_types(
 	id SERIAL PRIMARY KEY,
@@ -95,20 +102,21 @@ CREATE TABLE media_types(
     -- записей мало, поэтому индекс будет лишним (замедлит работу)!
 );
 
+
 DROP TABLE IF EXISTS media;
 CREATE TABLE media(
 	id SERIAL PRIMARY KEY,
     media_type_id BIGINT UNSIGNED,
     user_id BIGINT UNSIGNED NOT NULL,
   	body text,
-    filename VARCHAR(255),
-    `size` INT,
-	metadata JSON,
+    filename VARCHAR(255), -- для хранения ссылок на файл
+    `size` INT, -- размер файла
+	metadata JSON, -- можем хранить и имя файла и размер
     created_at DATETIME DEFAULT NOW(),
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (media_type_id) REFERENCES media_types(id) ON UPDATE CASCADE ON DELETE SET NULL
+    FOREIGN KEY (media_type_id) REFERENCES media_types(id) ON UPDATE CASCADE ON DELETE SET NULL -- при обновлении каскад при удалении сетнул
 );
 
 DROP TABLE IF EXISTS likes;
